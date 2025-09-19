d2d86ebba6a90d6dc5ad9813e53303a85eb702f9
Kostas Douzis <kostasdouzis@gmail.com>
2025-09-19 12:13:03 +0300

    Load rules from csv using DMN

--- src/main/java/com/example/drools_poc/RuleEngineDemo.java
+++ src/main/java/com/example/drools_poc/RuleEngineDemo.java
@@ -9,43 +9,43 @@ import java.util.Collection;
 
 public class RuleEngineDemo {
     public static void main(String[] args) throws Exception {
-//        CsvRuleEngine engine = new CsvRuleEngine();
-//        engine.loadRules("src/main/resources/tax-rules.csv");
-//
-//        Reservation summerTrip = new Reservation("PortA", "INBOUND", LocalDate.of(2025, 7, 15));
-//        Reservation winterTrip = new Reservation("PortA", "INBOUND", LocalDate.of(2025, 12, 20));
-//        Reservation marchTrip = new Reservation("PortA", "INBOUND", LocalDate.of(2025, 3, 20));
-//        Reservation outboundTrip = new Reservation("PortA", "OUTBOUND", LocalDate.of(2025, 3, 10));
-//        Reservation noDirection = new Reservation("PortA", LocalDate.of(2025, 3, 10));
-//
-//        Reservation portBInbounda = new Reservation("PortB", "INBOUND", LocalDate.of(2025, 3, 20));
-//        Reservation portBNoDirection = new Reservation("PortB", LocalDate.of(2025, 6, 20));
-//
-//        System.out.println("--------------PortA----------------------------");
-//        System.out.println("Summer trip tax: " + engine.evaluate(summerTrip));
-//        System.out.println("Winter trip tax: " + engine.evaluate(winterTrip));
-//        System.out.println("March trip tax: " + engine.evaluate(marchTrip));
-//        System.out.println("Outbound trip tax: " + engine.evaluate(outboundTrip));
-//        System.out.println("No direction trip tax: " + engine.evaluate(noDirection));
-//
-//        System.out.println("--------------PortB----------------------------");
-//        System.out.println("Inbound port B tax: " + engine.evaluate(portBInbounda));
-//        System.out.println("No direction port B tax: " + engine.evaluate(portBNoDirection));
+        CsvRuleEngine engine = new CsvRuleEngine();
+        engine.loadRules("src/main/resources/tax-rules.csv");
 
-        KieServices kieServices = KieServices.Factory.get();
-        KieContainer kieContainer = kieServices.getKieClasspathContainer();
-        KieSession kieSession = kieContainer.newKieSession("ksession-rules");
+        Reservation summerTrip = new Reservation("PortA", "INBOUND", LocalDate.of(2025, 7, 15));
+        Reservation winterTrip = new Reservation("PortA", "INBOUND", LocalDate.of(2025, 12, 20));
+        Reservation marchTrip = new Reservation("PortA", "INBOUND", LocalDate.of(2025, 3, 20));
+        Reservation outboundTrip = new Reservation("PortA", "OUTBOUND", LocalDate.of(2025, 3, 10));
+        Reservation noDirection = new Reservation("PortA", LocalDate.of(2025, 3, 10));
 
-// Example reservation
-        Reservation r = new Reservation("PortA", "INBOUND", LocalDate.of(2025, 7, 15));
-        kieSession.insert(r);
+        Reservation portBInbounda = new Reservation("PortB", "INBOUND", LocalDate.of(2025, 3, 20));
+        Reservation portBNoDirection = new Reservation("PortB", LocalDate.of(2025, 6, 20));
 
-        kieSession.fireAllRules();
+        System.out.println("--------------PortA----------------------------");
+        System.out.println("Summer trip tax: " + engine.evaluate(summerTrip));
+        System.out.println("Winter trip tax: " + engine.evaluate(winterTrip));
+        System.out.println("March trip tax: " + engine.evaluate(marchTrip));
+        System.out.println("Outbound trip tax: " + engine.evaluate(outboundTrip));
+        System.out.println("No direction trip tax: " + engine.evaluate(noDirection));
 
-        Collection<?> results = kieSession.getObjects(obj -> obj instanceof TaxResult);
-        for (Object obj : results) {
-            System.out.println(obj);
-        }
+        System.out.println("--------------PortB----------------------------");
+        System.out.println("Inbound port B tax: " + engine.evaluate(portBInbounda));
+        System.out.println("No direction port B tax: " + engine.evaluate(portBNoDirection));
+
+//        KieServices kieServices = KieServices.Factory.get();
+//        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+//        KieSession kieSession = kieContainer.newKieSession("ksession-rules");
+//
+//// Example reservation
+//        Reservation r = new Reservation("PortA", "INBOUND", LocalDate.of(2025, 7, 15));
+//        kieSession.insert(r);
+//
+//        kieSession.fireAllRules();
+//
+//        Collection<?> results = kieSession.getObjects(obj -> obj instanceof TaxResult);
+//        for (Object obj : results) {
+//            System.out.println(obj);
+//        }
     }
 }
 
